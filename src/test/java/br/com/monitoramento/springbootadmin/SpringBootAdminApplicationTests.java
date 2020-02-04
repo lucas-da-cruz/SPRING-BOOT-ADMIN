5e436142667d10ff0502f31d48eebaf8b5e7f085
Lucas Cruz <CruzLucas@JohnDeere.com>
2020-02-04 14:56:22 -0300

    Criação da aplicação

---
+++ src/test/java/br/com/monitoramento/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.monitoramento.springbootadmin;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
